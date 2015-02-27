c2d02f1a045dcba26bb7516b5660f7c6f6764873
Andre Roussakoff <madrus@gmail.com>
2015-02-27 07:23:35 +0100

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() { // log can be any name you like
-  gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources) // get source file
+    .pipe(coffee({ bare: true })) // and pipe it the the gulp-coffee plugin
+                                  // without the top-level function safety wrapper
+      .on('error', gutil.log)     // on error log the error message
+    .pipe(gulp.dest('components/scripts')) // move the newly created file to scripts folder
 });
