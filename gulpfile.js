123b2ae12bec5723856e4a80b1332ee321823df1
Andre Roussakoff <madrus@gmail.com>
2015-02-27 19:05:58 +0100

    gulp-concat added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrids.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
 
 gulp.task('coffee', function() {
-  gulp.src(coffeeSources) // get source file
+  gulp.src(coffeeSources) // source file(s)
     .pipe(coffee({ bare: true })) // and pipe it the the gulp-coffee plugin
                                   // without the top-level function safety wrapper
       .on('error', gutil.log)     // on error log the error message
     .pipe(gulp.dest('components/scripts')) // move the newly created file to scripts folder
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources) // source file(s)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
